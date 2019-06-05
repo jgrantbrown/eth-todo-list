cd5732752ca799ade3c68d2d1671b5351ba85920
Jason <j.grant.brown@gmail.com>
2019-06-04 23:19:07 -0400

    testing

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,27 @@
 pragma solidity ^0.5.0;
 
-contract TodoList{
-  uint public taskCount = 0;
+  contract TodoList{
+    uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+// Constructor function for contract
+  constructor()public{
+    createTask("This is a starter task");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] =  Task(taskCount, _content, false);
+  }
+
+
+
+
 }
